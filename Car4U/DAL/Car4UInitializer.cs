9c323acae486b01036061ee1db1b55fc62d5a303
Diogo <diogo.ramalho117@gmail.com>
2015-12-16 22:23:34 +0000

    FacebookShare a dar + initializer

---
+++ Car4U/DAL/Car4UInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Car4U.DAL
+{
+    public class Car4UInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext> 
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+
+        }
+    }
+}
