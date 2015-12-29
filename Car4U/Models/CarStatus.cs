6dbb66964e6b878381bad1b2bd8cc92bfeec432a
Diogo117 <Dogofour@hotmail.com>
2015-12-29 19:23:01 +0000

    fix do GearID + 2views

--- Car4U/Models/CarStatus.cs
+++ Car4U/Models/CarStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,13 @@ namespace Car4U.Models
     {
         public int ID { get; set; }
         public string Observation { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime BeginDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime FinishDate { get; set; }
 
 
