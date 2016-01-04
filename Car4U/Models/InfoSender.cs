a7306e9ed637db85426930d49f6fbe3f7f73867b
Diogo117 <Dogofour@hotmail.com>
2016-01-04 19:35:51 +0000

    Parametros para a reserva

---
+++ Car4U/Models/InfoSender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Car4U.Models
+{
+    public class InfoSender
+    {
+        //Este model não é, repito NÃO É, para aparecer na BD
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BeginDate { get; set; }
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime BeginHour { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EndDate { get; set; }
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime EndHour { get; set; }
+
+
+        public int Age { get; set; }
+
+
+        public int CategoryID { get; set; }
+        public int MPDeliveryID { get; set; }
+        public int MPReturnID { get; set; }
+
+
+    }
+}
