40427f85f2fc6cb21ab8ae45d16b1e11779e9ad4
André Maia <andremaia89@ua.pt>
2016-01-10 16:32:04 +0000

    Config + Initializer update

--- Car4U/DAL/Car4UInitializer.cs
+++ Car4U/DAL/Car4UInitializer.cs
@@ -178,45 +178,43 @@ namespace Car4U.DAL
 
             var mtypes = new List<ExtraModelType>
             {
-                new ExtraModelType {ID = 1, Description = "GPS" },
-                new ExtraModelType {ID = 2, Description = "Car Seat for Baby" },
-                new ExtraModelType {ID = 3, Description = "Via Verde" }
+                new ExtraModelType {ID = 1, Description = "GPS - Sistema de Navegação" },
+                new ExtraModelType {ID = 2, Description = "Assento de Criança" },
+                new ExtraModelType {ID = 3, Description = "Cadeira de Bébé" },
+                new ExtraModelType {ID = 4, Description = "Via Verde" }
             };
             mtypes.ForEach(s => context.ExtraModelTypes.Add(s));
             context.SaveChanges();
 
             var mod = new List<ExtraModel>
             {
-                new ExtraModel {ID = 1, ExtraModelTypeID = 1, Model = "GPS TPSI 1", Price = 50, Stock=2 },
-                new ExtraModel {ID = 2, ExtraModelTypeID = 2, Model = "Safe Seat 1", Price = 20, Stock=2 },
-                new ExtraModel {ID = 3, ExtraModelTypeID = 1, Model = "GPS TPSI 2", Price = 40, Stock=2 },
-                new ExtraModel {ID = 4, ExtraModelTypeID = 1, Model = "GPS TPSI 3", Price = 35, Stock=2 },
-                new ExtraModel {ID = 5, ExtraModelTypeID = 1, Model = "GPS TPSI 4", Price = 10, Stock=2 },
-                new ExtraModel {ID = 6, ExtraModelTypeID = 2, Model = "Safe Seat 2", Price = 20, Stock=2 },
-                new ExtraModel {ID = 7, ExtraModelTypeID = 2, Model = "Safe Seat 3", Price = 20, Stock=2 },
-                new ExtraModel {ID = 8, ExtraModelTypeID = 3, Model = "Via Verde", Price = 10, Stock=2 },
+                new ExtraModel {ID = 1, ExtraModelTypeID = 1, Model = "GPS", Price = 4.99, Stock = 3 },
+                new ExtraModel {ID = 2, ExtraModelTypeID = 2, Model = "Assento 8-12 anos", Price = 9.90, Stock = 3 },
+                new ExtraModel {ID = 3, ExtraModelTypeID = 2, Model = "Cadeira 0-7 anos", Price = 9.90, Stock = 3 },
+                new ExtraModel {ID = 4, ExtraModelTypeID = 2, Model = "Identificador Via Verde", Price = 9.90, Stock = 3 }
             };
             mod.ForEach(s => context.ExtraModels.Add(s));
             context.SaveChanges();
 
             var item = new List<ExtraItem>
             {
+                //GPS - Sistema de Navegação - GPS - 3 Items
                 new ExtraItem {ID = 1, ExtraModelID = 1 },
                 new ExtraItem {ID = 2, ExtraModelID = 1 },
-                new ExtraItem {ID = 3, ExtraModelID = 2 },
+                new ExtraItem {ID = 3, ExtraModelID = 1 },
+                //Assento de Criança - Assento 8-12 - 3 items
                 new ExtraItem {ID = 4, ExtraModelID = 2 },
-                new ExtraItem {ID = 6, ExtraModelID = 3 },
+                new ExtraItem {ID = 5, ExtraModelID = 2 },
+                new ExtraItem {ID = 6, ExtraModelID = 2 },
+                //Cadeira de Bébé - Cadeira 0-7 - 3 items
                 new ExtraItem {ID = 7, ExtraModelID = 3 },
-                new ExtraItem {ID = 8, ExtraModelID = 4 },
-                new ExtraItem {ID = 9, ExtraModelID = 4 },
-                new ExtraItem {ID = 10, ExtraModelID = 5 },
-                new ExtraItem {ID = 11, ExtraModelID = 5 },
-                new ExtraItem {ID = 12, ExtraModelID = 6 },
-                new ExtraItem {ID = 13, ExtraModelID = 6 },
-                new ExtraItem {ID = 14, ExtraModelID = 7 },
-                new ExtraItem {ID = 15, ExtraModelID = 7 },
-                new ExtraItem {ID = 16, ExtraModelID = 8 },
-                new ExtraItem {ID = 17, ExtraModelID = 8 },
+                new ExtraItem {ID = 8, ExtraModelID = 3 },
+                new ExtraItem {ID = 9, ExtraModelID = 3 },
+                //Via Verde - Identificador Via Verde - 3 items
+                new ExtraItem {ID = 9, ExtraModelID = 3 },
+                new ExtraItem {ID = 10, ExtraModelID = 3 },
+                new ExtraItem {ID = 11, ExtraModelID = 3 },
+
             };
             item.ForEach(s => context.ExtraItems.Add(s));
             context.SaveChanges();
